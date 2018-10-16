f3457fb975fa9d57b20681b9a7b5d2eacac9a4fe
Virginia Dooley <vcdooley@gmail.com>
2018-10-16 09:53:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,8 +28,8 @@ class Song
   end 
   
   def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
+    genre_count = {} 
+    @@genres.each do |genre| 
       if genre_count[genre] #if genre 
         genre_count[genre] += 1 
       else
